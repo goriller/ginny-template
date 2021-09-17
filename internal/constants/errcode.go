cda690c54a9d3bb688829121e9bc5c499c3633ab
linyyyang <linyyyang@tencent.com>
2021-09-17 11:09:44 +0800

    🐞 fix:规范错误码

--- internal/constants/errcode.go
+++ internal/constants/errcode.go
@@ -2,12 +2,22 @@ package constants
 
 // ErrCode
 const (
-	ERR_GETINFO = 1000
+	SUCCESS        = 0
+	FAIL           = 1
+	PARAMS_INVALID = 2
+	NOT_FOUND      = 3
+	INTERNAL_ERR   = 4
+	UNAUTHORIZED   = 5
 )
 
 // ErrMap
 var ErrMap = map[int]string{
-	1000: "获取信息失败",
+	0: "成功",
+	1: "失败",
+	2: "参数不合法",
+	3: "请求的资源不存在",
+	4: "服务器内部错误",
+	5: "非法访问",
 }
 
 // GetErrMsg
